ae2e5bf9c0bff6e83c456b67807d658f1b72db98
pavel.sinkevich@gmail.com <pavel.sinkevich@gmail.com@9c32f4ac-3783-e91a-a416-ff040f5373b0>
2012-06-06 22:46:20 +0000

    Initial code upload

---
+++ trunk/ sql-geometry-analysis-library/GraphsAnalysisFunctions/AverageGraph.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using Microsoft.SqlServer.Types;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static SqlGeometry AverageGraph(SqlGeometry sourceGraph)
+    {
+        SqlGeometry result = new SqlGeometry();
+        int dotQty = Convert.ToInt32(sourceGraph.STNumPoints());
+        // For each dot, find union of all dots with same X.
+
+        return result;
+    }
+};
+
